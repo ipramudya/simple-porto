d7875af58e1308832f20a5aef0f04ff2bc9e44c7
ipramudya <ipramudya.dev@gmail.com>
2023-05-20 14:43:57 +0700

    feat: i18n

--- src/components/dialog-trigger.tsx
+++ src/components/dialog-trigger.tsx
@@ -9,10 +9,15 @@ interface Props {
 
 const DialogTrigger: FunctionComponent<Props> = ({ icon }) => {
     const [isOpen, setIsOpen] = useState(false)
+    const [, lang] = location.pathname.split("/")
+
+    const getModifiedPath = (lang: string) => {
+        const [, , ...restPaths] = location.pathname.split("/")
+        return `/${lang}/`.concat(restPaths.join("/"))
+    }
 
     const onButtonClicked = () => {
         setIsOpen((prev) => !prev)
-        console.log("runs")
     }
 
     return (
@@ -30,8 +35,11 @@ const DialogTrigger: FunctionComponent<Props> = ({ icon }) => {
             {isOpen && (
                 <div class="absolute right-0 top-[3rem] min-w-[132px] overflow-hidden rounded-md">
                     <div class="flex flex-col">
-                        <button class="flex items-center justify-between space-x-2 bg-dark-5 p-2 hover:bg-dark-4">
-                            <span>English</span>
+                        <a
+                            href={lang === "en" ? getModifiedPath("id") : getModifiedPath("en")}
+                            class="flex items-center justify-between space-x-2 bg-dark-5 p-2 hover:bg-dark-4"
+                        >
+                            <span>{lang === "en" ? "Indonesia" : "English"}</span>
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="24"
@@ -44,9 +52,9 @@ const DialogTrigger: FunctionComponent<Props> = ({ icon }) => {
                                     d="M14.022 7h1a1.001 1.001 0 0 1 1 1v1a1 1 0 0 0 2 0V8a3.003 3.003 0 0 0-3-3h-1a1 1 0 0 0 0 2Zm-4 9h-1a1.001 1.001 0 0 1-1-1v-1a1 1 0 0 0-2 0v1a3.003 3.003 0 0 0 3 3h1a1 1 0 0 0 0-2Zm11-1a1 1 0 0 0 0-2h-3v-.5a1 1 0 0 0-2 0v.5h-3a1 1 0 0 0 0 2h5.184a6.728 6.728 0 0 1-1.225 2.527a6.668 6.668 0 0 1-.63-.983a1 1 0 1 0-1.779.912a8.678 8.678 0 0 0 .96 1.468a6.618 6.618 0 0 1-2.426 1.099a1 1 0 0 0 .427 1.954a8.635 8.635 0 0 0 3.445-1.622a8.724 8.724 0 0 0 3.469 1.622a1 1 0 1 0 .43-1.954a6.725 6.725 0 0 1-2.446-1.09A8.736 8.736 0 0 0 20.244 15Zm-11.97-3.757a1 1 0 0 0 1.94-.486l-1.757-7.03a2.281 2.281 0 0 0-4.426 0l-1.757 7.03a1 1 0 0 0 1.94.486L5.552 9h2.94ZM6.052 7l.698-2.787a.291.291 0 0 1 .544 0L7.991 7Z"
                                 />
                             </svg>
-                        </button>
+                        </a>
                         <button class="flex items-center justify-between space-x-2 bg-dark-5 p-2 hover:bg-dark-4">
-                            <span>Light Mode</span>
+                            <span>Mode</span>
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="24"
---
+++ src/i18n/ui.ts
@@ -0,0 +1,23 @@
+export const defaultLang = "en"
+
+export const languages = {
+    en: "English",
+    id: "Indonesia",
+} as const
+
+export const ui = {
+    en: {
+        /* navbar */
+        "nav.home": "Home",
+        "nav.projects": "Projects",
+        "nav.about": "About",
+        "nav.contact": "Contact",
+    },
+    id: {
+        /* navbar */
+        "nav.home": "Beranda",
+        "nav.projects": "Proyek",
+        "nav.about": "Tentang",
+        "nav.contact": "Kontak",
+    },
+} as const
