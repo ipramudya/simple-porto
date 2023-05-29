933eb1b1b3456c8cfe888b7f3f47a305905ad053
ipramudya <ipramudya.dev@gmail.com>
2023-05-29 20:12:12 +0700

    build: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import preact from "@astrojs/preact"
 import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
 
-// https://astro.build/config
 export default defineConfig({
+    site: "https://pramudyatamir.web.app",
     integrations: [
         preact({ compat: true }),
         tailwind(),
