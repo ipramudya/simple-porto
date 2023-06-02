f3e5bc4763caab9b3015ff97fd5d71df58f3e1e5
ipramudya <ipramudya.dev@gmail.com>
2023-06-02 13:21:08 +0700

    feat: footer

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -12,6 +12,9 @@ export const ui = {
         "nav.projects": "Projects",
         "nav.about": "About",
         "nav.contact": "Connect",
+        "footer.0": "Crafted by Pramudya. Build with ",
+        "footer.1": " & written in",
+        "footer.2": ", deployed on",
     },
     id: {
         /* navbar */
@@ -19,5 +22,8 @@ export const ui = {
         "nav.projects": "Proyek",
         "nav.about": "Tentang",
         "nav.contact": "Kontak",
+        "footer.0": "Dibuat oleh Pramudya. Dibangun menggunakan",
+        "footer.1": " & ditulis dalam",
+        "footer.2": ", dideploy pada",
     },
 } as const
