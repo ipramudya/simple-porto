559a90127c859069bc03ec6a98af785e7b432eba
ipramudya <ipramudya.dev@gmail.com>
2024-07-29 12:38:42 +0700

    tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,35 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	darkMode: "class",
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
+			},
+			colors: {
+				brand: {
+					1: "#dfcebc",
+					2: "#9e764c",
+				},
+				dark: {
+					1: "#EDEDED",
+					2: "#A0A0A0",
+					3: "#7E7E7E",
+					4: "#707070",
+					5: "#505050",
+					6: "#3E3E3E",
+					7: "#343434",
+					8: "#2E2E2E",
+					9: "#282828",
+					10: "#232323",
+					11: "#1C1C1C",
+					12: "#161616",
+				},
+			},
+		},
 	},
 	plugins: [],
-}
+};
