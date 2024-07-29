e20410a46b67cf1090194696780119c71d76ca80
ipramudya <ipramudya.dev@gmail.com>
2024-07-29 13:22:52 +0700

    layout & container

---
+++ .prettierrc.mjs
@@ -0,0 +1,17 @@
+/** @type {import("prettier").Config} */
+export default {
+    tabWidth: 4,
+    printWidth: 100,
+    singleQuote: true,
+    trailingComma: 'all',
+    semi: false,
+    plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+    overrides: [
+        {
+            files: '*.astro',
+            options: {
+                parser: 'astro',
+            },
+        },
+    ],
+}
