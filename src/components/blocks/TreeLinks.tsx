aaf27a23841ac879d3bb94924a350fa35bf2fd33
ipramudya <ipramudya.dev@gmail.com>
2024-07-31 20:07:20 +0700

    fix forgotten href

--- src/components/blocks/TreeLinks.tsx
+++ src/components/blocks/TreeLinks.tsx
@@ -5,7 +5,7 @@ export default function TreeLinks() {
     return (
         <div className="flex flex-col space-y-6 pt-[2rem]">
             {DataTreeLinks.map((tlink) => (
-                <ShineLink key={tlink.href} className="min-h-[48px] min-w-[300px]" text={tlink.label}>
+                <ShineLink key={tlink.href} className="min-h-[48px] min-w-[300px]" text={tlink.label} href={tlink.href}>
                     <tlink.icon />
                 </ShineLink>
             ))}
